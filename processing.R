67b1b928a264ccc42217e12f5165a4a129e9b359
Art83 <shvetsov1983@gmail.com>
2024-04-30 19:11:56 +1000

    feat: added family history of AZ

--- processing.R
+++ processing.R
@@ -16,5 +16,55 @@ ss <- demo %>%
   
 
 
-apply(ss, 2, table)  
-apply(apply(ss, 2, is.na), 2, sum)  
+# Family history
+adni3_1 <- read.csv("D:/AZ/adni/raw/family/adni3_fam_2.csv", stringsAsFactors = F)
+adni_r_1 <- read.csv("D:/AZ/adni/raw/family/adni1_2_go_fam_2.csv", stringsAsFactors = F)
+
+
+
+
+  
+
+
+adni3 <- adni3_1 %>%
+  filter(VISCODE == "sc") %>%
+  select(RID,MOTHDEM, MOTHAD, FATHDEM, FATHAD) %>%
+  rename(FHQMOM = MOTHDEM,
+         FHQMOMAD = MOTHAD,
+         FHQDAD = FATHDEM,
+         FHQDADAD = FATHAD) %>%
+  filter(!is.na(FHQMOM),
+         !is.na(FHQDAD)) %>%
+  filter(FHQMOM %in% c(0,1),
+         FHQDAD %in% c(0,1)) %>%
+  mutate(across(c(FHQMOMAD, FHQDADAD), 
+                ~ replace(., is.na(.) | . < 0, 0))) %>%
+  filter(FHQMOMAD %in% c(0,1),
+         FHQDADAD %in% c(0,1)) %>%
+  mutate(mother = FHQMOM + FHQMOMAD,
+         father = FHQDAD + FHQDADAD) %>%
+  select(RID, mother, father)
+
+
+adnir <- adni_r_1 %>%
+  filter(VISCODE == "sc") %>%
+  select(RID, FHQMOM, FHQMOMAD, FHQDAD, FHQDADAD) %>%
+  filter(!is.na(FHQMOM),
+         !is.na(FHQDAD)) %>%
+  filter(FHQMOM %in% c(0,1),
+         FHQDAD %in% c(0,1)) %>%
+  mutate(across(c(FHQMOMAD, FHQDADAD), 
+                ~ replace(., is.na(.) | . < 0, 0))) %>%
+  filter(FHQMOMAD %in% c(0,1),
+         FHQDADAD %in% c(0,1)) %>%
+  mutate(mother = FHQMOM + FHQMOMAD,
+         father = FHQDAD + FHQDADAD) %>%
+  select(RID, mother, father)
+
+
+family <- rbind(as.data.frame(adni3),
+                as.data.frame(adnir))
+  
+  
+  
+
